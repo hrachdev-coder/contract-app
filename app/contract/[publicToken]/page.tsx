26a27c1b3b0dfbdc1c0eeff18c25c40d2d65ea60
hrachdev-coder <hrach.dev@gmail.com>
2026-03-27 14:57:25 +0400

    adding  contract and  templates

--- app/contract/[publicToken]/page.tsx
+++ app/contract/[publicToken]/page.tsx
@@ -1,3 +1,5 @@
+export const dynamic = "force-dynamic";
+
 import { notFound } from "next/navigation";
 import { createServiceClient } from "@/lib/supabase/service";
 import ContractReviewClient from "../ContractReviewClient";
